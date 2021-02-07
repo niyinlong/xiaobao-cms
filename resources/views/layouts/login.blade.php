7de931e89929b389791e2f2dcaf0a1f58dc98e9f
倪银龙 <niyinlong@126.com>
2021-02-07 16:59:11 +0800

    登录页面

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -8,52 +8,31 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link href="{{ asset('layui/css/layui.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app" class="layui-layout">
-        <nav class="layui-header header">
-            <div class="layui-container">
-                <a class="logo" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+<body class="layui-layout-body">
+    <div class="layui-layout layui-layout-admin">
+        <div class="layui-header">
+            <a class="layui-logo" href="{{ route('index') }}">{{ config('app.name', 'Laravel') }}</a>   
+            <ul class="layui-nav layui-layout-right">
+                @guest
+                    @if (Route::has('login'))
+                        <li class="layui-nav-item {{Request::getPathInfo()=='/login'?'layui-this':''}}">
+                            <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
 
-                <div class="collapse navbar-collapse" >
-                    <!-- Right Side Of Navbar -->
-                    <ul class="layui-nav">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="layui-nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="layui-nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="layui-nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        @yield('content')
+                    @if (Route::has('register'))
+                        <li class="layui-nav-item {{Request::getPathInfo()=='/register'?'layui-this':''}}">
+                            <a  href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="layui-nav-item">
+                        <a href="javascript:;">{{ Auth::user()->name }}</a>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
+    @yield('content')
 </body>
 </html>
